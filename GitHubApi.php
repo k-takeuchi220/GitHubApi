e2d4395c645aa30e4e7eb1bdc36d9315615a28b4
katsushi takeuchi <tk1997220@gmail.com>
2021-10-16 03:15:20 +0900

    100件以上も対応するよう修正

--- GitHubApi.php
+++ GitHubApi.php
@@ -3,17 +3,36 @@
 class GitHubApi
 {
     protected $token;
+
     protected $userId;
+
+    // [userId][page_perPage] => followers
+    protected $_cacheFollowers;
+
+    // [userId][page_perPage] => followings
+    protected $_cacheFollowings;
 
     protected const GIT_API_URL = 'https://api.github.com/';
     protected const FOLLOWERS_URL_FORMAT = self::GIT_API_URL."users/%s/followers";
     protected const FOLLOWING_URL_FORMAT = self::GIT_API_URL."users/%s/following";
     protected const FOLLOW_URL_FORMAT = self::GIT_API_URL."user/following/%s";
 
+    // 使えないらしい
+    // protected const CHECK_FOLLOW_URL_FORMAT = self::GIT_API_URL."user/%s/following/%s";
+    
+    protected const MAX_PER_PAGE = 100;
+    
     public function __construct(string $token, string $userId)
     {
         $this->token = $token;
         $this->userId = $userId;
+    }
+
+    //======================================== 自身に対する処理 ========================================/
+
+    public function getUserId(): string
+    {
+        return $this->userId;
     }
 
     /**
@@ -37,10 +56,10 @@ class GitHubApi
     /**
      * 片思いフォロー一覧取得
      */
-    public function getUnrequitedFollowings(): array
+    public function getUnrequitedFollowings(string $userId, int $maxPage = 10): array
     {
-        $followings = $this->getFollowings();
-        $followers = $this->getFollowers();
+        $followings = $this->getAllFollowingsByUserId($userId, $maxPage);
+        $followers = $this->getAllFollowersByUserId($userId, $maxPage);
 
         return array_diff($followings, $followers);
     }
@@ -48,27 +67,12 @@ class GitHubApi
     /**
      * 片思われフォロー一覧取得
      */
-    public function getUnrequitedFollowers(): array
+    public function getUnrequitedFollowers(string $userId, int $maxPage = 10): array
     {
-        $followings = $this->getFollowings();
-        $followers = $this->getFollowers();
+        $followings = $this->getAllFollowingsByUserId($userId, $maxPage);
+        $followers = $this->getAllFollowersByUserId($userId, $maxPage);
 
         return array_diff($followers, $followings);
-    }
-
-    /**
-     * フォロワー一覧取得
-     */
-    public function getFollowers(int $page = 1, int $perPage = 100): array
-    {
-        $userId = $this->userId;
-        $url = sprintf(self::FOLLOWERS_URL_FORMAT, $userId);
-
-        $query = ['page' => $page, 'per_page' => $perPage];
-        $datas = $this->curl($url, $this->token, 'GET', $query);
-
-        $followers = array_column($datas, 'login');
-        return $followers;
     }
 
     /**
@@ -76,14 +80,122 @@ class GitHubApi
      */
     public function getFollowings(int $page = 1, int $perPage = 100): array
     {
-        $userId = $this->userId;
-        $url = sprintf(self::FOLLOWING_URL_FORMAT, $userId);
+        $userId = $this->getUserId();
+        $followings = $this->getFollowingsByUserId($userId, $page, $perPage);
+        return $followings;
+    }
 
+    /**
+     * フォロワー一覧取得
+     */
+    public function getFollowers(int $page = 1, int $perPage = 100): array
+    {
+        $userId = $this->getUserId();
+        $followers = $this->getFollowersByUserId($userId, $page, $perPage);
+        return $followers;
+    }
+
+    /**
+     * 全てのフォロー一覧取得
+     * default 上限10ページ 1000ユーザまで
+     */
+    public function getAllFollowings(int $maxPage = 10): array
+    {
+        $userId = $this->getUserId();
+        $followings = $this->getAllFollowingsByUserId($userId, $maxPage);
+        return $followings;
+    }
+
+    /**
+     * 全てのフォロワー一覧取得
+     * default 上限10ページ 1000ユーザまで
+     */
+    public function getAllFollowers(int $maxPage = 10): array
+    {
+        $userId = $this->getUserId();
+        $followers = $this->getAllFollowersByUserId($userId, $maxPage);
+        return $followers;
+    }
+
+    //======================================== 共通処理 ========================================//
+
+    /**
+     * 該当ユーザのフォロ一覧取得
+     */
+    public function getFollowingsByUserId(string $userId, int $page = 1, int $perPage = 100): array
+    {
+        $pageKey = $this->makePageKey($page, $perPage);
+        if (!empty($_cacheFollowings) && isset($_cacheFollowings[$userId]) && isset($_cacheFollowings[$userId][$pageKey])) {
+            return $_cacheFollowings[$userId];
+        }
+        $url = sprintf(self::FOLLOWING_URL_FORMAT, $userId);
         $query = ['page' => $page, 'per_page' => $perPage];
         $datas = $this->curl($url, $this->token, 'GET', $query);
-
         $followings = array_column($datas, 'login');
+        $this->_cacheFollowings[$userId][$pageKey] = $followings;
+
         return $followings;
+    }
+    
+    /**
+     * 該当ユーザのフォロワー一覧取得
+     */
+    public function getFollowersByUserId(string $userId, int $page = 1, int $perPage = 100): array
+    {
+        $pageKey = $this->makePageKey($page, $perPage);
+        if (!empty($_cacheFollowers) && isset($_cacheFollowers[$userId]) && isset($_cacheFollowers[$userId][$pageKey])) {
+            return $_cacheFollowers[$userId][$pageKey];
+        }
+        $url = sprintf(self::FOLLOWERS_URL_FORMAT, $userId);
+        $query = ['page' => $page, 'per_page' => $perPage];
+        $datas = $this->curl($url, $this->token, 'GET', $query);
+        $followers = array_column($datas, 'login');
+        $this->_cacheFollowers[$userId][$pageKey] = $followers;
+
+        return $followers;
+    }
+
+    /**
+     * 該当ユーザの全てのフォロー一覧取得
+     * default 上限10ページ 1000ユーザまで
+     */
+    public function getAllFollowingsByUserId(string $userId, int $maxPage = 10): array
+    {
+        $followings = [];
+        $perPage = self::MAX_PER_PAGE;
+        for ($page = 1; $page <= $maxPage; ++$page) {
+            $followingsPage = $this->getfollowingsByUserId($userId, $page, $perPage);
+            if (empty($followingsPage)) {
+                break;
+            }
+            $followings = array_merge($followings, $followingsPage);
+        }
+
+        return $followings;
+    }
+
+    /**
+     * 該当ユーザの全てのフォロワー一覧取得
+     * default 上限10ページ 1000ユーザまで
+     */
+    public function getAllFollowersByUserId(string $userId, int $maxPage = 10): array
+    {
+        $followers = [];
+        $perPage = self::MAX_PER_PAGE;
+        for ($page = 1; $page <= $maxPage; ++$page) {
+            $followersPage = $this->getFollowersByUserId($userId, $page, $perPage);
+            if (empty($followersPage)) {
+                break;
+            }
+            $followers = array_merge($followers, $followersPage);
+        }
+
+        return $followers;
+    }
+
+    protected function makePageKey(int $page, int $perPage): string
+    {
+        return $page.'_'.$perPage;
     }
 
     protected function curl(string $url, string $token, string $type, array $query = []): array
